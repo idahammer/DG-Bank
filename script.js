bbf9fb222b0bbd65a51dc99a3f7d26f41360cf29
Johan Anderson <johan@obsolete.se>
2016-05-12 15:34:31 +0200

    added some code for moving the button

--- script.js
+++ script.js
@@ -2,41 +2,35 @@ $(document).ready(function() {
 	// Stuff here runs when the sketch is ready for action.
 	// This is when we wire-up events, like the following.
 
-	// Here, we wire up the 'click' event for the selector
-	// #clickMe (which corresponds to an element with id 'clickMe')
-	// to a function named 'clickHandler' (which we define later)
-	$("#clickMe").on("click", clickHandler);
-
   // Prevent normal iOS/Android touch gestures
   $('body').on('touchmove', function(e) {
     e.preventDefault();
   });
 
   // Important! Initialise Hammer
-  var hammertime = new Hammer($('.swipearea').get(0));
+  var hammertime = new Hammer($('#swipearea').get(0));
 
   // Listen for 'swipe' events
   hammertime.on('swipe', onSwipe);
 });
 
-function clickHandler() {
-  alert("Yep, you clicked the button in DG-Bank!");
-}
-
 // Change color based on velocity
 function onSwipe(e) {
   var max = 6; // The highest allowed velocity value
   var percentage = Math.abs(e.velocityX) / max;
+  var w = $("#swipearea").width();
+  console.log(w);
 
   // Make sure we don't exceed 1
   percentage = Math.min(percentage, 1);
+  var px = w * percentage;
+  $("#swipebutton").offset({left: px});
   if(percentage > 0.8)
   {
     // Snap to 1 when over 90%.
     percentage = 1;
     // Succefully unlocked! do something!
-    $(".swipearea").text($(".swipearea").data("target"));
-		document.location.href = $(".swipearea").data("target");
+		document.location.href = $("#swipearea").data("target");
   }
 
   // Convert velocity (which is now 0.0-1.0) to the rgb scale.
@@ -49,7 +43,7 @@ function onSwipe(e) {
 
   // Set our red amount to the background
   // and use 0 for the green and blue values
-  $('.swipearea').css({
+  $('#swipearea').css({
     'background-color': 'rgb('+ red +',0,0)'
 
   });
